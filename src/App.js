3291eed18000169a4fe27c5560081aa42fc337ac
Antonio-RF <antoniidaressurreicao@gmail.com>
2025-11-03 13:44:17 -0300

    Modification on router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <header className="top-nav">
         <img src={logo} alt="Logo" className="nav-logo" />
         <nav>
